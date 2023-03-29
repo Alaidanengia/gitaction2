c80cc750e1b81f6427ea69a255dad9c1ecd6ad3d
Ala <75151204+Alaidanengia@users.noreply.github.com>
2023-03-28 21:46:01 -0700

    third commit

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -1,13 +1,22 @@
 # The configuration for the `remote` backend.
-
-backend "s3" {
+terraform {
+  required_version = "~> 1.0"         #1.1.4/5/6/7   1.2/3/4/5 1.1.4/5/6/7
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+  backend "s3" {
     bucket = "my-terraformstate-test34"
-    key = "terraform/terraform.tfstate"
-#     dynamodb_table = "terraform-lock"
+    key    = "prod/terraform.tfstate"
+    region = "us-east-1"
+    #dynamodb_table = "terraform-lock"
+  }
+}
 
-    region = "us-west-1"
 
- }
+
 
 
 resource "aws_s3_bucket" "my_bucket" {
